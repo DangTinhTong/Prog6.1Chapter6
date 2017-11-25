8973ea43673efe063c7da1c7a96bb2846c001911
admin <dangtinhtong@gmail.com>
2017-11-26 00:09:58 +0700

    first commit

--- prog6.1/main.m
+++ prog6.1/main.m
@@ -2,16 +2,24 @@
 //  main.m
 //  prog6.1
 //
-//  Created by admin on 11/26/17.
-//  Copyright © 2017 admin. All rights reserved.
-//
+//  Created by Tống Đăng Tình on 11/26/17.
+//  Copyright © 2017 Tống Đăng Tình. All rights reserved.
+//-------------------------------------------------------
+// Calculate the absolute value of an integer
+
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        int number;
+        NSLog(@"Type in your number:");
+        scanf("%i: ",&number);
+        if (number<0)
+            number=-number;
+        NSLog(@"The absolute value is %i ",number);
     }
     return 0;
 }
